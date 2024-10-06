402203b2c24a383fef728349906c413da0768354
Niyaki Pham <niyakipham@gmail.com>
2024-10-06 16:55:12 +0700

    Update wall.py

--- wall.py
+++ wall.py
@@ -2,9 +2,6 @@ import discord, asyncio, aiohttp
 import pandas as pd
 from discord.ext import commands
 
-# ID của kênh mà bot sẽ chỉ cho phép gửi ảnh
-CHANNEL_ID = 1291045191687475230 # Thay thế bằng ID kênh của bạn
-
 # Khởi tạo intents
 intents = discord.Intents.default()
 intents.message_content = True
@@ -12,14 +9,14 @@ intents.message_content = True
 # Tạo một instance bot
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-@bot.event
-async def on_ready():
-    print(f'Bot đã đăng nhập với tên: {bot.user}')
+# ID của kênh mà bot sẽ chỉ cho phép gửi ả
+# Gộp các ID kênh vào danh sách
+CHANNEL_IDS = [1291045191687475230, 1289573372782444674]  # WALL và MEME
 
 @bot.event
 async def on_message(message):
     # Nếu tin nhắn không được gửi trong kênh đã chỉ định hoặc là tin nhắn từ bot thì bỏ qua
-    if message.channel.id != CHANNEL_ID or message.author == bot.user:
+    if message.channel.id not in CHANNEL_IDS or message.author == bot.user:
         return
 
     # Kiểm tra nếu tin nhắn chứa file đính kèm và tất cả đều là ảnh
@@ -33,5 +30,8 @@ async def on_message(message):
         # Nếu tin nhắn không chứa file đính kèm thì xóa tin nhắn
         await message.delete()
         await message.channel.send(f"{message.author.mention}, chỉ được gửi hình ảnh trong kênh này!", delete_after=5)
+
+    # Đừng quên gọi process_commands để xử lý các lệnh khác
+    await bot.process_commands(message)
 
 bot.run('MTI5MjM5ODAyODg4NzU2MDE5Mw.GzlcYC.orxY4aPlyk3H5eEPb3p867mvlHPs0G2UJaHbm0')

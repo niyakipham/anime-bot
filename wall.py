4c34dc384e9950c504da87005881ac155a45c6b3
Niyaki Pham <niyakipham@gmail.com>
2024-10-06 15:12:05 +0700

    Update wall.py

--- wall.py
+++ wall.py
@@ -5,6 +5,13 @@ from discord.ext import commands
 # ID của kênh mà bot sẽ chỉ cho phép gửi ảnh
 CHANNEL_ID = 1291045191687475230 # Thay thế bằng ID kênh của bạn
 
+# Khởi tạo intents
+intents = discord.Intents.default()
+intents.message_content = True
+
+# Tạo một instance bot
+bot = commands.Bot(command_prefix='!', intents=intents)
+
 @bot.event
 async def on_ready():
     print(f'Bot đã đăng nhập với tên: {bot.user}')
@@ -27,4 +34,4 @@ async def on_message(message):
         await message.delete()
         await message.channel.send(f"{message.author.mention}, chỉ được gửi hình ảnh trong kênh này!", delete_after=5)
 
-bot.run('MTI4OTUyODk5NzA4ODA2NzYwNg.G9R7e3.dlVut9wb5CFjYInBF-YGmSRhcIss6_OnNUat28')
+bot.run('MTI5MjM5ODAyODg4NzU2MDE5Mw.GzlcYC.orxY4aPlyk3H5eEPb3p867mvlHPs0G2UJaHbm0')

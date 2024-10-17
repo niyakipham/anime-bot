e68ce21d47bec302d1ec15fc24596c23c5d3fe6d
Niyaki Pham <niyakipham@gmail.com>
2024-10-17 21:43:20 +0700

    qsq

--- night.py
+++ night.py
@@ -9,27 +9,27 @@ from discord.ext import tasks
 
 WALL = 1295293677828309032
 # Tạo client Discord với prefix và intents đầy đủ quyền
-client = discord.Client(intents=discord.Intents.all())
+bot = discord.Bot(intents=discord.Intents.all())
 
 # Sự kiện khi bot đã sẵn sàng
-@client.event
+@bot.event
 async def on_ready():
-    print(f'Bot đã đăng nhập với tên: {client.user}')
+    print(f'Bot đã đăng nhập với tên: {bot.user}')
     # Khởi động task gửi ảnh mỗi 1 phút
     send_image_task.start()
 
 # Sự kiện khi bot nhận tin nhắn
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
 
 @tasks.loop(minutes=1)
 async def send_image_task():
     # Kiểm tra nếu bot đã sẵn sàng
-    if client.is_ready():
+    if bot.is_ready():
         # Lấy kênh bằng ID
-        channel = client.get_channel(TARGET_CHANNEL_ID)
+        channel = bot.get_channel(TARGET_CHANNEL_ID)
         if channel:
             neko_image_url = nekos.img('wallpaper')  # Lấy ảnh từ endpoint 'neko'
             await channel.send(f"Tự động gửi ảnh mỗi phút:\n{neko_image_url}")
